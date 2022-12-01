4e125361cc3bd189589c9a030236214de154979b
maaryaahmed <maarya251@gmail.com>
2022-12-01 13:40:53 -0700

    sql stuff

--- MovieTheaterTicketReservationApp/src/ManageMovie.java
+++ MovieTheaterTicketReservationApp/src/ManageMovie.java
@@ -5,13 +5,8 @@ import java.sql.*;
 
 public class ManageMovie 
 {
-    /**
-     * setting the url, username, password and table name to a final value, hard coded it into the fil
-     */
-    private final String DBURL = "jdbc:mysql://localhost/movie_theatres";
-    private final String USERNAME = "ensf480";
-    private final String PASSWORD = "ensf480";
-    private final String TABLE_NAME = "Movie";
+
+    private String TABLE_NAME = "Movie";
     private Connection dbConnect;
     private ResultSet results;
 
@@ -20,46 +15,9 @@ public class ManageMovie
 
     public ManageMovie(){
         this.loginInstance = LoginInstance.getInstance();
-        initializeDriver();
-        initializeConnection();
     }
 
-    /**
-     * initializing the driver or throws the exception if driver is not found
-     */
-    public void initializeDriver() {
-        try {
-            Class.forName ("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println ("Driver not found! Check output console");
-            e.printStackTrace ();
-        }
-    }
-
-    /**
-     * initializing the connection and throwing exception if failed
-     */
-    public void initializeConnection() {
-        try {
-            dbConnect = DriverManager.getConnection (DBURL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            System.out.println ("Connection Failed! Check output console");
-            e.printStackTrace ();
-        }
-    }
-
-    /**
-     * disconnecting the connection 
-     */
-    public void disconnectConnection() {
-        try {
-            dbConnect.close ();
-        } catch (SQLException e) {
-            System.out.println ("Disconnection Failed! Check output console");
-        }
-    }
-
-
+    
     public void addMovie(int mvID, String mvName, String openDate, int theID, int showRoomID, int showTimeID, String shownAt){
         // SQL 
         // add movie to 
