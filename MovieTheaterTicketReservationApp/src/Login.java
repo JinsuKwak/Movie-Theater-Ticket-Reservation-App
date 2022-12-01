dd97bf9a68e22e007476c1c4035bc3ac4bb34c59
Jinsu Kwak <jinsukwak2001@gmail.com>
2022-11-30 20:08:10 -0700

    add Login control

--- MovieTheaterTicketReservationApp/src/Login.java
+++ MovieTheaterTicketReservationApp/src/Login.java
@@ -1,3 +1,38 @@
 public class Login {
+    private String eneteredUserID;
+    private String eneteredUserPW;
+    private LoginInstance loginInstance;
+    private User user;
+    private boolean isAdmin;
+    private boolean isLoggedIn = false;
+
+    public Login(String uID, String uPW){
+        this.eneteredUserID = uID;
+        this.eneteredUserPW = uPW;
+        this.loginInstance = LoginInstance.getInstance();
+    }
+
+    public boolean validate(String eneteredUserID, String eneteredUserPW){
+        // pseudocode
+
+        // result = JDBC.getResult();
+        // if(result is notvalid){
+        //     return isLoggedIn;
+        // }
+        
+        // isLoggedIn = true;
+        // if(result.isAdmin == true){
+        //     this.isAdmin = true;
+        //     this.user = new AdminUser(eneteredUserPW, eneteredUserPW, result.fName, result.Lname, isAdmin);
+        // } else {
+        //     this.user = new RegisteredUser(eneteredUserPW, eneteredUserPW, result.fName, result.Lname, isAdmin, eneteredUserPW)
+        // }
     
+        return isLoggedIn;
+    }
+
+    public void updateInstance(){
+        this.loginInstance.changeStatus(user, isAdmin, isLoggedIn);
+    }
+
 }
--- MovieTheaterTicketReservationApp/src/LoginInstance.java
+++ MovieTheaterTicketReservationApp/src/LoginInstance.java
@@ -1,3 +1,51 @@
+import java.util.Date;
+
+import javax.print.attribute.standard.Sides;
+
 public class LoginInstance {
+    private static LoginInstance loginInstance;
+    private User user;
+    private boolean isAdmin;
+    private boolean isLoggedIn;
+    private Date logInTime;
+
+    private LoginInstance(){
+       this.isAdmin = false;
+       this.isLoggedIn = false;
+       this.logInTime = new Date();         // at miliseconds
+    }
     
+    public void setInstance(LoginInstance loginInstance){
+        LoginInstance.loginInstance = loginInstance;
+    }
+
+    public static LoginInstance getInstance(){
+        if(loginInstance == null){
+            loginInstance = new LoginInstance();
+        }
+        return loginInstance;
+    }
+
+    public void changeStatus(User user, boolean isAdmin, boolean isLoggedIn){
+        this.user = user;
+        this.isAdmin = isAdmin;
+        this.isLoggedIn = isLoggedIn;
+    }
+
+    public User getUser(){
+        return this.user;
+    }
+
+    public boolean getIsAdmin(){
+        return this.isAdmin;
+    }
+
+    public boolean getIsLoggedIn(){
+        return this.isLoggedIn;
+    }
+
+    public Date getLogInTime(){
+        return this.logInTime;
+    }
+
 }
