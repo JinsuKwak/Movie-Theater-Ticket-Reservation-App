2cb14a05143f70709eb26660fc335a73ea10d2ef
Nour Amer <84950398+NoureldinAmer@users.noreply.github.com>
2022-12-03 21:40:23 -0700

    implement observer pattern classes

---
+++ MovieTheaterTicketReservationApp/src/MovieNews.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class MovieNews implements Observer{
+    WeeklyNews weeklyNews;
+
+
+    public MovieNews(WeeklyNews weeklyNews) {
+        this.weeklyNews = weeklyNews;
+    }
+
+
+    @Override
+    public void update(ArrayList<Movie> movies) {
+        ArrayList<User> recipients; //sql statement to get the registered users
+        for(Movie movie : movies) {
+            Email.sendMovieNews(movie.getMovieName(), movie.getMoviePrice(), movie.getOpeningDate().toString(), recipients);
+        }
+    }
+}
