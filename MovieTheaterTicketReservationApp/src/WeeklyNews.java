2b6691e3c7e9709e58f737dc10d6afa9b516955b
Nour Amer <84950398+NoureldinAmer@users.noreply.github.com>
2022-12-05 04:03:30 -0700

    implement 'WeeklyNews' class

--- MovieTheaterTicketReservationApp/src/WeeklyNews.java
+++ MovieTheaterTicketReservationApp/src/WeeklyNews.java
@@ -4,63 +4,109 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 import java.util.Date;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.*;
 
-public class WeeklyNews extends SQLController implements Subject{
+public class WeeklyNews implements Subject {
     private ArrayList<Observer> observers;
     private ArrayList<Movie> movies;
+    private final String DBURL = "jdbc:mysql://localhost/movie_theatres";
+    private final String USERNAME = "ensf480";
+    private final String PASSWORD = "ensf480";
+    private final String TABLE_NAME = "Movie";
+    private Connection dbConnect;
+    private ResultSet results;
 
     public WeeklyNews(ArrayList<Observer> observers, ArrayList<Movie> movies) {
-        super("jdbc:mysql://localhost/movie_theatres", "ensf480", "ensf480");
-        movies = new ArrayList<>();
-        observers = new ArrayList<>();
+        initializeDriver();
+    }
+
+    /**
+     * initializing the connection and throwing exception if failed
+     */
+    public void initializeDriver() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * initializing the connection and throwing exception if failed
+     */
+    public void initializeConnection() {
+        try {
+            dbConnect = DriverManager.getConnection (DBURL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println ("Connection Failed! Check output console");
+            e.printStackTrace ();
+        }
+    }
+
+    /**
+     * disconnecting the connection
+     */
+    public void disconnectConnection() {
+        try {
+            dbConnect.close();
+        } catch (SQLException e) {
+            System.out.println("Disconnection Failed! Check output console");
+            e.printStackTrace();
+        }
     }
 
     private void startAnnualFee() {
+        initializeConnection();
         Timer timer = new Timer();
         TimerTask task = new TimerTask() {
-
-            // load all RU, check user != Admin
             @Override
             public void run() {
-                Date todaysDate = new Date();
-                String sql = "SELECT * FROM Movie WHERE openinfDate > ?";
-                PreparedStatement p = this.dbConnection.prepareStatement(sql);
-                p.setDate(1, todaysDate);
-                ResultSet result = p.executeQuery();
-                
-                while(result.next()) {
-                    Movie movie = new Movie(
-                        result.getInt("movieID"), 
-                        result.getString("movieName"),
-                        result.getDate("openingDate"),
-                        result.getDouble("moviePrice"),
-                        result.getInt("theatreID")
-                    );
-                    movies.add(movie);
+                try {
+                    Date todaysDate = new Date();
+                    java.sql.Date sqlDate = new java.sql.Date(todaysDate.getTime());
+                    String sql = "SELECT * FROM Movie WHERE openinfDate > ?";
+                    PreparedStatement p = dbConnect.prepareStatement(sql);
+                    p.setDate(1, sqlDate);
+                    ResultSet result = p.executeQuery();
+
+                    while (result.next()) {
+                        Movie movie = new Movie(
+                                result.getInt("movieID"),
+                                result.getString("movieName"),
+                                result.getDate("openingDate"),
+                                result.getDouble("moviePrice"),
+                                result.getInt("theatreID"));
+                        movies.add(movie);
+                    }
+                    notifyAllObservers();
+                    disconnectConnection();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                notifyAllObservers();
+
             }
         };
-        timer.scheduleAtFixedRate(task, new Date(), TimeUnit.DAYS.toMillis(7) ); 
+        timer.scheduleAtFixedRate(task, new Date(), TimeUnit.DAYS.toMillis(7));
     }
 
     @Override
     public void registerObserver(Observer o) {
         observers.add(o);
-        
     }
 
     @Override
     public void removeObserver(Observer o) {
         observers.remove(o);
-        
+
     }
 
     @Override
     public void notifyAllObservers() {
-        for(Observer observer : observers) {
+        for (Observer observer : observers) {
             observer.update(movies);
         }
     }
